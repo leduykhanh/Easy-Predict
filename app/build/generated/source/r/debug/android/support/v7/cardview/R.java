7dde45f91ac887798e8c9ff047aff5e7f61fc201
Jangkoo <leejangkoo@gmail.com>
2017-02-11 02:34:10 +0800

    rm

--- app/build/generated/source/r/debug/android/support/v7/cardview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/cardview/R.java
@@ -32,22 +32,25 @@ public final class R {
 		public static final int cardview_default_radius = 0x7f08004f;
 	}
 	public static final class style {
-		public static final int CardView = 0x7f0900b1;
-		public static final int CardView_Dark = 0x7f0900b2;
-		public static final int CardView_Light = 0x7f0900b3;
+		public static final int Base_CardView = 0x7f090089;
+		public static final int CardView = 0x7f090080;
+		public static final int CardView_Dark = 0x7f0900b3;
+		public static final int CardView_Light = 0x7f0900b4;
 	}
 	public static final class styleable {
-		public static final int[] CardView = { 0x7f01002c, 0x7f01002d, 0x7f01002e, 0x7f01002f, 0x7f010030, 0x7f010031, 0x7f010032, 0x7f010033, 0x7f010034, 0x7f010035, 0x7f010036 };
-		public static final int CardView_cardBackgroundColor = 0;
-		public static final int CardView_cardCornerRadius = 1;
-		public static final int CardView_cardElevation = 2;
-		public static final int CardView_cardMaxElevation = 3;
-		public static final int CardView_cardPreventCornerOverlap = 5;
-		public static final int CardView_cardUseCompatPadding = 4;
-		public static final int CardView_contentPadding = 6;
-		public static final int CardView_contentPaddingBottom = 10;
-		public static final int CardView_contentPaddingLeft = 7;
-		public static final int CardView_contentPaddingRight = 8;
-		public static final int CardView_contentPaddingTop = 9;
+		public static final int[] CardView = { 0x0101013f, 0x01010140, 0x7f01002c, 0x7f01002d, 0x7f01002e, 0x7f01002f, 0x7f010030, 0x7f010031, 0x7f010032, 0x7f010033, 0x7f010034, 0x7f010035, 0x7f010036 };
+		public static final int CardView_android_minHeight = 1;
+		public static final int CardView_android_minWidth = 0;
+		public static final int CardView_cardBackgroundColor = 2;
+		public static final int CardView_cardCornerRadius = 3;
+		public static final int CardView_cardElevation = 4;
+		public static final int CardView_cardMaxElevation = 5;
+		public static final int CardView_cardPreventCornerOverlap = 7;
+		public static final int CardView_cardUseCompatPadding = 6;
+		public static final int CardView_contentPadding = 8;
+		public static final int CardView_contentPaddingBottom = 12;
+		public static final int CardView_contentPaddingLeft = 9;
+		public static final int CardView_contentPaddingRight = 10;
+		public static final int CardView_contentPaddingTop = 11;
 	}
 }
--- app/build/generated/source/r/debug/com/google/android/gms/ads/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/ads/R.java
@@ -114,7 +114,7 @@ public final class R {
 		public static final int store_picture_title = 0x7f060079;
 	}
 	public static final class style {
-		public static final int Theme_IAPTheme = 0x7f090106;
+		public static final int Theme_IAPTheme = 0x7f090107;
 	}
 	public static final class styleable {
 		public static final int[] AdsAttrs = { 0x7f01001f, 0x7f010020, 0x7f010021 };
